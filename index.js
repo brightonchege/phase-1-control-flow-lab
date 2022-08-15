5ec9b7d808d53d1efa0556c61d3f48c17701707b
brightonchege <brightonspak@gmail.com>
2022-08-15 22:12:42 +0300

    if,else and switch complete

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+ 
+   if(ride <= 400){
+       return "This one is on me!"
+        }
+   if(ride > 2500 ){  
+        return "No can do."
+       }
+   if (ride >= 2000){
+      return "I will gladly take your thirty bucks."
+     }
+}
+scuberGreetingForFeet(ride)
+
+
+    function ternaryCheckCity(city){
+
+     if(city === 'NYC'){
+      return 'Ok, sounds good.'
+     }
+     else{
+      return 'No go.'
+       }
+     }
+ternaryCheckCity(city)
+
+
+function switchOnCharmFromTip(tip){
+
+switch(tip){
+  case "generous":
+  return "Thank you so much."
+
+  case "not as generous":
+  return "Thank you."
+
+  case "thanks for everything":
+  return "Bye."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  }
+switchOnCharmFromTip(tip)
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
